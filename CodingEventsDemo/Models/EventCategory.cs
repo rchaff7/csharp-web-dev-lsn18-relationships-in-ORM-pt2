a8faa753e36c70b9c01688dcedcd5eafe5aeec59
Ryan Chaffee <rchaff7@yahoo.com>
2021-01-02 14:02:35 -0600

    Add project files.

---
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingEventsDemo.Models
+{
+    public class EventCategory
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public List<Event> events { get; set; }
+
+        public EventCategory(string name)
+        {
+            Name = name;
+        }
+
+        public EventCategory()
+        {
+        }
+    }
+}
