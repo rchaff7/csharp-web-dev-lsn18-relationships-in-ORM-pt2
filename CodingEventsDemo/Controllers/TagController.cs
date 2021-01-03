cc1e986858fdb48aecada07ecb4bb77fa66122a9
Ryan Chaffee <rchaff7@yahoo.com>
2021-01-02 23:50:45 -0600

    add Tag to Event - 20.5.5 complete

--- CodingEventsDemo/Controllers/TagController.cs
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -1,5 +1,6 @@
 ﻿using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -39,6 +40,35 @@ namespace CodingEventsDemo.Controllers
             }
 
             return View("Add", tag);
+        }
+
+        // responds to URLs like /Tag/AddEvent/5 (where 5 is an event ID)
+        public IActionResult AddEvent(int id)
+        {
+            Event theEvent = context.Events.Find(id);
+            List<Tag> possibleTags = context.Tags.ToList();
+
+            AddEventTagViewModel viewmodel = new AddEventTagViewModel(theEvent, possibleTags);
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventTagViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventTag eventTag = new EventTag
+                {
+                    EventId = viewModel.EventId,
+                    TagId = viewModel.TagId
+                };
+                context.EventTags.Add(eventTag);
+                context.SaveChanges();
+
+                return Redirect("/Events/Detail/" + viewModel.EventId);
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ CodingEventsDemo/ViewModels/AddEventTagViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventTagViewModel
+    {
+        [Required]
+        public int EventId { get; set; }
+        [Required]
+        public int TagId { get; set; }
+        public Event Event { get; set; }
+        public List<SelectListItem> Tags { get; set; }
+
+
+        public AddEventTagViewModel()
+        {
+        }
+
+        public AddEventTagViewModel(Event theEvent, List<Tag> possibleTags)
+        {
+            Tags = new List<SelectListItem>();
+            foreach(var tag in possibleTags)
+            {
+                Tags.Add(new SelectListItem
+                {
+                    Value = tag.Id.ToString(),
+                    Text = tag.Name
+                });
+            }
+
+            Event = theEvent;
+        }
+    }
+}
